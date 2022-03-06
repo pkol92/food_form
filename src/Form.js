218e2626b7c44cd08ab9f6d03b637068d3a47478
pkol92 <paulina_kolasa@wp.pl>
2022-03-06 17:29:46 +0100

    add submit function

--- src/Form.js
+++ src/Form.js
@@ -1,20 +1,54 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef } from 'react';
 
-const Form = () => {
+const Form = (props) => {
     const [dish, setDish] = useState("pizza");
+    const [food, setFood] = useState(props.food);
 
     useEffect(() => {
         setDish(dish);
-    }, [dish])
+    }, [dish]);
+
+    const submit = e => {
+        e.preventDefault()
+        console.log(food);
+        // fetch('/api', {
+        //     method: 'POST',
+        //     body: JSON.stringify({ food }),
+        //     headers: { 'Content-Type': 'application/json' },
+        //   })
+        //     .then(res => res.json())
+        //     .then(json => setFood(json.food))
+    };
+    
     
     return ( 
-        <form>
-            <input name="name" type="text" placeholder="Dish name" required></input>
-            <label for="preparation_time">Preparation time:</label>
-            <input id="preparation_time" name="preparation_time" type="time" step="2" required></input>
-            <label for="dish_type">Choose a dish:</label>
-            <select name="dish_type" id="dish_type" required onChange={(e)=> setDish(e.target.value)}>
+        <form onSubmit={submit}>
+            <input 
+                name="food[name]" 
+                type="text" 
+                placeholder="Dish name" 
+                required
+                onChange={e => setFood({ ...food, name: e.target.value })}
+            />
+
+            <label htmlFor="preparation_time">Preparation time:</label>
+            <input 
+                id="preparation_time" 
+                name="food[preparation_time]" 
+                type="time" 
+                step="2" 
+                required
+                onChange={e => setFood({ ...food, preparation_time: e.target.value })}
+            />
+
+            <label htmlFor="dish_type">Choose a dish:</label>
+            <select 
+                name="food[dish_type]" 
+                id="dish_type" 
+                required 
+                onChange={(e)=> { setDish(e.target.value); setFood({ ...food, dish_type: e.target.value })}}
+                >
                 <option value="pizza">Pizza</option>
                 <option value="soup">Soup</option>
                 <option value="sandwich">Sandwich</option>
@@ -22,24 +56,49 @@ const Form = () => {
 
             {(dish === "pizza") ? 
             <div>
-                <label for="no_of_slices">Number of Slices:</label>
-                <input id="no_of_slices" name = "no_of_slices" type="number" required></input>
-                <label for="diameter">Diameter:</label>
-                <input id="diameter" name="diameter" type="number"></input>
+                <label htmlFor="no_of_slices">Number of Slices:</label>
+                <input 
+                    id="no_of_slices" 
+                    name = "food[no_of_slices]" 
+                    type="number" 
+                    required
+                    onChange={e => setFood({ ...food, no_of_slices: e.target.value })}
+                />
+                <label htmlFor="diameter">Diameter:</label>
+                <input 
+                    id="diameter" 
+                    name="food[diameter]" 
+                    type="number"
+                    onChange={e => setFood({ ...food, diameter: e.target.value })}
+                />
             </div>
             : null }
 
             {(dish === "soup") ? 
             <div>
-                <label for="spiciness_scale">Spiciness scale (1-10):</label>
-                <input id="spiciness_scale" name = "spiciness_scale" type="number" required min="1" max="10"></input>
+                <label htmlFor="spiciness_scale">Spiciness scale (1-10):</label>
+                <input 
+                    id="spiciness_scale" 
+                    name = "food[spiciness_scale]" 
+                    type="number" 
+                    required 
+                    min="1" 
+                    max="10"
+                    onChange={e => setFood({ ...food, spiciness_scale: e.target.value })}
+                />
             </div>
             : null }
 
             {(dish === "sandwich") ? 
             <div>
-                <label for="slices_of_bread">Slices of bread:</label>
-                <input id="slices_of_bread" name = "slices_of_bread" type="number" required></input>
+                <label htmlFor="slices_of_bread">Slices of bread:</label>
+                <input 
+                    id="slices_of_bread" 
+                    name = "food[slices_of_bread]" 
+                    type="number" 
+                    required
+                    onChange={e => setFood({ ...food, slices_of_bread: e.target.value })}
+                />
             </div>
             : null }
 
